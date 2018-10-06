b422a8612dae26090c5c02bae3fa8cfa7d0c1926
Andrea <andreamammoliti@yahoo.it>
2018-10-06 23:15:58 +0800

    Working version of Consuming API App

--- geo/views.py
+++ geo/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    response = requests.get('http://api.ipstack.com/check?access_key=2cf547973c9f834bbb2032dbddd4a926')
-    geodata = response.json() 
+    response = requests.get('http://api.ipstack.com/check?access_key=YourOwnKey')
+    data = response.json() 
     return render(request, 'geo/home.html', {
-        'ip': geodata['ip'],
-        'country': geodata['country_name']
+        'ip': data['ip'],
+        'country': data['country_name'],
+        'continent': data['continent_name'],
+        'zip': data['zip'],
+        'capital':data['location']['capital'],
+        'flag': data['location']['country_flag']
     })
  
     
