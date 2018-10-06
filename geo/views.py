b4300c43e804438a992e399bdfd1fad43ae8a43d
Andrea Mammoliti <andreamammoliti@yahoo.it>
2018-10-06 23:38:47 +0800

    Update views.py

--- geo/views.py
+++ geo/views.py
@@ -5,12 +5,14 @@ def home(request):
     response = requests.get('http://api.ipstack.com/check?access_key=YourOwnKey')
     data = response.json() 
     return render(request, 'geo/home.html', {
-        'ip': data['ip'],
+        #'ip': data['ip'],
         'country': data['country_name'],
         'continent': data['continent_name'],
+        'region_name' : data ['region_name'],
         'zip': data['zip'],
         'capital':data['location']['capital'],
         'flag': data['location']['country_flag']
     })
  
+    
     
